5c8226c9780d8118696b1556db33b9047a0a208b
GulcanC <gulcancoskun.a@gmail.com>
2025-03-24 15:31:37 +0100

    add

---
+++ src/VariableTut.java
@@ -0,0 +1,19 @@
+public class VariableTut { // this is class
+    static int m=100; // this is static variable
+
+    void method() {
+        int n=90; // local variable
+    }
+    public static void main(String[] args) { // this is method
+        int data=50; // instance variable
+
+        // addition of two numbers
+        int a =10;
+        int b=20;
+        int c=a+b;
+        int d=b-a;
+        //write sout + enter
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
---
+++ src/dataTypes.java
@@ -0,0 +1,16 @@
+public class dataTypes {
+    // declare main method
+    public static void main(String[] args) {
+        Boolean one = true;
+        System.out.println(one);
+        short s =10000;
+        short r =-5000;
+        System.out.println(s+ " "+r);
+        float f1=2.3999f;
+        System.out.println(f1);
+        int a = 260;
+        System.out.println(a);
+        int z= 23;
+
+    }
+}
