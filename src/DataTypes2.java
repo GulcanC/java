0f57cc79f4be787476c97413026a0929c16f115f
GulcanC <gulcancoskun.a@gmail.com>
2025-05-21 10:41:31 +0200

    data types

---
+++ src/DataTypes2.java
@@ -0,0 +1,12 @@
+public class DataTypes2 {
+    // declare your main method
+    public static void main(String[] args) {
+        Boolean x =true;
+        float f = 2.50f;
+        int a = 234;
+        System.out.println(x);
+        System.out.println(f);
+        System.out.println(a);
+
+    }
+}
