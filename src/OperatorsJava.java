90d6d2999c6f00a7306a5415626d21607b52fb85
GulcanC <gulcancoskun.a@gmail.com>
2025-05-23 15:13:38 +0200

    string

--- src/OperatorsJava.java
+++ src/OperatorsJava.java
@@ -1,2 +1,56 @@
+
+import java.util.Scanner;
 public class OperatorsJava {
+    public static void main(String[] args) {
+        // create Scanner object to get input value from system
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter first number : ");
+        int FirstNumber = scn.nextInt();
+
+        System.out.println("Enter second number : ");
+        int SecondNumber = scn.nextInt();
+
+        System.out.println(FirstNumber + " " + SecondNumber);
+
+        // Unary operation
+        System.out.println(++FirstNumber);
+        int x = 10;
+        System.out.println(--x);
+        // Arithmetic operations
+        System.out.println(10+SecondNumber);
+        // Left shift operator
+        System.out.println(FirstNumber<<3);
+        System.out.println(5<<5); // 5*(2^5) => 5*32=160
+        // Right shift operator
+        System.out.println(22>>3); // 22/(2^3) => 22/8 = 2 => T gives integer not decimal result
+        // logical operators =>  && AND, || OR, ! NOT
+
+        int a=10;
+        int b=20;
+        int c=40;
+
+        System.out.println(a<b && c<a); // TRUE && FALSE => FALSE
+        System.out.println(a<b && b<c); // TRUE && TRUE => TRUE
+
+        System.out.println(a<b || c<b); // TRUE || FALSE => TRUE
+
+        System.out.println(a != 10); // FALSE
+        System.out.println(a != 23); // TRUE
+
+        // BITWISE OPERATORS => & AND, | OR
+
+        System.out.println(a<b && c<a); // TRUE && FALSE => FALSE
+        System.out.println(a<b && b<c); // TRUE && TRUE => TRUE
+
+        System.out.println(a<b || c<b); // TRUE || FALSE => TRUE
+
+        // Assignment operator
+        System.out.println(a+=4); // a+4 = 14
+        System.out.println(b-=4); // b-4 = 16
+        System.out.println(c*=2); // c*2 = 80
+
+
+
+
+    }
 }
---
+++ src/StringInJava.java
@@ -0,0 +1,24 @@
+public class StringInJava
+{
+    public static void main(String[] args) {
+
+        // create array of characters
+
+        char[] ch = { 'h', 'e', 'l', 'l', 'o'};
+        // create a string
+        // it concatinates all the characters and store inside object "s"
+
+        String s=new String(ch);
+        System.out.println(s);
+
+        // toLowerCase
+        String s1 = "HELLO WORLD";
+        System.out.println(s1.toLowerCase());
+
+
+
+
+
+
+    }
+}
